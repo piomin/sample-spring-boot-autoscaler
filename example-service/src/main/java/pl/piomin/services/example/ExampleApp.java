b1172f130b556267e15b0c696e89748198069f88
piotr.minkowski <piotr.minkowski@gmail.com>
2018-09-14 11:54:56 +0200

    Initial commit

---
+++ example-service/src/main/java/pl/piomin/services/example/ExampleApp.java
@@ -0,0 +1,13 @@
+package pl.piomin.services.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleApp.class, args);
+    }
+
+}
---
+++ example-service/src/main/java/pl/piomin/services/example/controller/ExampleController.java
@@ -0,0 +1,20 @@
+package pl.piomin.services.example.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/example")
+public class ExampleController {
+
+//    @Value("${spring.application.instanceId}")
+    private String instanceId;
+
+    @GetMapping
+    public String test() {
+       return "I'm " + instanceId;
+    }
+
+}
