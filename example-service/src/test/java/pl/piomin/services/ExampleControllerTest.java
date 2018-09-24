ba35fcbaaac40aedeb44db1291739d8a7fbab423
piotr.minkowski <piotr.minkowski@gmail.com>
2018-09-24 12:35:59 +0200

    adding empty test

---
+++ example-service/src/test/java/pl/piomin/services/ExampleControllerTest.java
@@ -0,0 +1,13 @@
+package pl.piomin.services;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ExampleControllerTest {
+
+    @Test
+    public void test() throws Exception {
+        Assert.assertTrue(true);
+    }
+
+}
