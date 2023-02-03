5217f6aeeeaa015951c86945da279cf3e25ec45f
piomin <piotr.minkowski@gmail.com>
2023-02-03 09:29:09 +0100

    enable auto-update

--- example-service/src/test/java/pl/piomin/services/ExampleControllerTest.java
+++ example-service/src/test/java/pl/piomin/services/ExampleControllerTest.java
@@ -2,7 +2,12 @@ package pl.piomin.services;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@SpringBootTest
+@RunWith(SpringRunner.class)
 public class ExampleControllerTest {
 
     @Test
